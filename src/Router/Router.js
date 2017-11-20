dd8b6deccddfaa595617c45493e7067225b265ab
Mike Stone <mike@gnar.dog>
2017-11-20 16:51:32 -0500

    Enable trailing commas

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import App from 'components/App';
 import Home from 'pages/Home';
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import logo from 'logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    const {children} = this.props;
+    const { children } = this.props;
 
     return (
       <div className="App">
