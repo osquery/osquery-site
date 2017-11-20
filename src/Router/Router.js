7e6b47827664d9d40b2ba9878422640539516c13
Mike Stone <mike@gnar.dog>
2017-11-20 16:24:17 -0500

    Update directory structure

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
-import App from 'App';
-import Home from 'Home';
+import App from 'components/App';
+import Home from 'pages/Home';
 
 class Router extends Component {
   render() {
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import Icon from 'components/Icon';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p className="home__intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
