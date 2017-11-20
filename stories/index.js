b77e75e40686f0b08c70e70c2a066dca33ff7210
Mike Stone <mike@gnar.dog>
2017-11-20 18:10:53 -0500

    Group platform icons together

--- stories/index.js
+++ stories/index.js
@@ -3,24 +3,54 @@ import { storiesOf } from '@storybook/react';
 
 import Icon from '../src/components/Icon';
 
+const IconWrapper = (Component) => {
+  class IconWrapperComponent extends React.Component {
+    static icons = [
+      'apple',
+      'centos',
+      'linux',
+      'ubuntu',
+      'windows'
+    ];
+
+    state = {
+      name: 'apple',
+    };
+
+    render() {
+      return (
+        <div>
+          <div>
+            {IconWrapperComponent.icons.map((platform, idx) => {
+              return (
+                <button
+                  key={idx}
+                  onClick={(e) => this.setState({ name: e.target.value })}
+                  value={platform}
+                >
+                  {platform}
+                </button>
+              );
+            })}
+          </div>
+          <br />
+          <Component {...this.props} name={this.state.name} />
+        </div>
+      );
+    }
+  }
+
+  return IconWrapperComponent;
+}
+
+const WrappedIcon = IconWrapper(Icon)
+
 storiesOf('Icon', module)
-  .add('apple', () => (
-    <Icon name="apple" />
-  ))
-  .add('centos', () => (
-    <Icon name="centos" />
+  .add('platform icons', () => (
+    <WrappedIcon />
   ))
   .add('githubMark', () => (
     <Icon name="githubMark" />
-  ))
-  .add('linux', () => (
-    <Icon name="linux" />
-  ))
-  .add('ubuntu', () => (
-    <Icon name="ubuntu" />
-  ))
-  .add('windows', () => (
-    <Icon name="windows" />
   ))
   .add('octocat', () => (
     <Icon name="octocat" />
