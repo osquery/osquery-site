e5d8ffdb7acf92470f601189d8d5bc2db6f861b8
Taylor Kearns <tkearns@gmail.com>
2018-05-22 13:29:56 -0400

    Add new indicator to schema tables
    If a table has been added since the prior schema version, we will see an indicator on the table listing as well as the table of contents entry.

--- src/components/SchemaTOC/TOCEntry/TOCEntry.js
+++ src/components/SchemaTOC/TOCEntry/TOCEntry.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import classnames from 'classnames'
-import { bool, func, string } from 'prop-types'
+import { bool, func, shape, string } from 'prop-types'
 
 import './TOCEntry.css'
 
 const baseClass = 'toc-entry'
 
-const TOCEntry = ({ active, children, entry, index, onClick }) => {
+const TOCEntry = ({ active, children, index, onClick, table }) => {
   const liClasses = classnames(baseClass, { [`${baseClass}--active`]: active })
 
   return (
-    <li className={liClasses} data-index={index} id={`toc-entry-${entry}`}>
-      <a className={`${baseClass}__link`} href={`#${entry}`} onClick={onClick}>
+    <li className={liClasses} data-index={index} id={`${baseClass}-${table.name}`}>
+      <a className={`${baseClass}__link`} href={`#${table.name}`} onClick={onClick}>
         {children}
       </a>
+
+      {table.new && (
+        <span>
+          <span>{` `}</span>
+          <span className={`${baseClass}__new-indicator`}>new</span>
+        </span>
+      )}
     </li>
   )
 }
@@ -21,8 +28,11 @@ const TOCEntry = ({ active, children, entry, index, onClick }) => {
 TOCEntry.propTypes = {
   active: bool,
   children: string.isRequired,
-  entry: string.isRequired,
   onClick: func.isRequired,
+  table: shape({
+    name: string,
+    new: bool,
+  }).isRequired,
 }
 
 export default TOCEntry
