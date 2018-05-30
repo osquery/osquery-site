bf1982da9890c2a7ed3e5fb87a2547772c69e035
Taylor Kearns <taylorkearns@users.noreply.github.com>
2018-05-30 12:31:59 -0400

    Blog images (#70)
    * Allow for local images in blog posts
    
    * Clean up BlogIndex.js
    
    * Add images to Z. Wasserman's blog post

---
+++ src/data/blog/posts/images/index.js
@@ -0,0 +1,13 @@
+// ****************************************************
+// Import images here, and export the image identifier.
+// ****************************************************
+
+import microscopeMan from './microscope-man-2018-05-30.svg'
+import osqueryTerminalListeningPorts from './osquery-terminal-listening-ports-2018-05-30.png'
+import osqueryTextLogo from './osquery-text-logo-2018-05-30.png'
+
+export default {
+  microscopeMan,
+  osqueryTerminalListeningPorts,
+  osqueryTextLogo,
+}
