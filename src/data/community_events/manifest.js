77c92d90069c1545e38733b766c67f76e38f5fe7
Jason Meller <jason.meller@gmail.com>
2018-10-02 10:15:59 -0400

    Remove canceled NYC workshop from events

--- src/data/community_events/manifest.js
+++ src/data/community_events/manifest.js
@@ -1,3 +1,3 @@
 // Add your event's filename to this list
 
-export default ['querycon18', 'kolide-osquery-workshop', 'kolide-osquery-workshop2']
+export default ['querycon18', 'kolide-osquery-workshop']
