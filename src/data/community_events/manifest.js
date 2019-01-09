3720e4482ba44306085c4ca2c5dd33ea90a97c37
Guillaume Ross <gross@groots-MBP.baconflavor.lan>
2019-01-09 12:16:20 -0500

    Adding 2 events
    Adding Art Into Science talk and BSidesSF workshop

--- src/data/community_events/manifest.js
+++ src/data/community_events/manifest.js
@@ -1,3 +1,3 @@
 // Add your event's filename to this list
 
-export default ['querycon18', 'kolide-osquery-workshop']
+export default ['querycon18', 'kolide-osquery-workshop', 'ACoD2019', 'BSidesSF2019']
