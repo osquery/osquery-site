4e7c92a9584fbcf61b49ee524be8be6da115ba53
Guillaume Ross <44678469+gross-uptycs@users.noreply.github.com>
2019-01-10 08:31:37 -0500

    Adding two events (#114)

--- src/data/community_events/manifest.js
+++ src/data/community_events/manifest.js
@@ -1,3 +1,3 @@
 // Add your event's filename to this list
 
-export default ['querycon18', 'kolide-osquery-workshop']
+export default ['querycon18', 'kolide-osquery-workshop', 'acod2019', 'bsidessf2019']
