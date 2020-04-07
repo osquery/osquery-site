03039d86c1109ae15f9d92cecb0d14318610dfd6
Zachary Wasserman <zachwass2000@gmail.com>
2020-04-07 10:00:44 -0700

    Allow linking to schema tables without osquery version
    Fixes #51

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -19,62 +19,61 @@ class Router extends Component {
         <App>
           <Switch>
             <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
-
             <Route
               exact
               path={`${process.env.PUBLIC_URL}/blog/official-news`}
               render={props => <BlogIndex {...props} blogType="official-news" />}
             />
-
             <Route
               exact
               path={`${process.env.PUBLIC_URL}/blog/community-articles`}
               render={props => <BlogIndex {...props} blogType="community-articles" />}
             />
-
             <Redirect
               exact
               from={`${process.env.PUBLIC_URL}/blog`}
               to={`${process.env.PUBLIC_URL}/blog/official-news`}
             />
-
             <Route path={`${process.env.PUBLIC_URL}/blog/:blog_title`} component={BlogShow} />
-
             <Redirect
               exact
               from={`${process.env.PUBLIC_URL}/news`}
               to={`${process.env.PUBLIC_URL}/blog/official-news`}
             />
-
             <Redirect
               exact
               from={`${process.env.PUBLIC_URL}/community`}
               to={`${process.env.PUBLIC_URL}/blog/community-articles`}
             />
-
             <Redirect exact from={`${process.env.PUBLIC_URL}/docs/tables`} to="/schema" />
-
             <Route
               path={`${process.env.PUBLIC_URL}/downloads/:release_type/:osquery_version`}
               component={Downloads}
             />
-
             <Redirect
               from={`${process.env.PUBLIC_URL}/downloads`}
               to={`${process.env.PUBLIC_URL}/downloads/official/${currentOsqueryVersion}`}
             />
-
             <Route
               exact
               path={`${process.env.PUBLIC_URL}/schema/:schemaVersion`}
               component={Schema}
             />
-
-            <Redirect
-              from={`${process.env.PUBLIC_URL}/schema`}
-              to={`${process.env.PUBLIC_URL}/schema/${currentOsqueryVersion}`}
+            <Route
+              from={`${process.env.PUBLIC_URL}/schema/`}
+              render={({ location }) => (
+                // This render function is a redirect that preserves the search
+                // and hash params. This allows linking directly to tables
+                // without knowing the version number in advance.
+                <Redirect
+                  to={{
+                    pathname: `${process.env.PUBLIC_URL}/schema/${currentOsqueryVersion}/`,
+                    search: location.search,
+                    hash: location.hash,
+                  }}
+                />
+              )}
             />
-
             <Route component={NotFound} />
           </Switch>
         </App>
