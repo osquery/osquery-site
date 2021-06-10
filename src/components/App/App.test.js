2361a9c5304cc76b8e169157dd2f2b2fbf660875
seph <seph@directionless.org>
2021-06-09 22:35:55 -0400

    tests, as completely minimal as they are

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -5,11 +5,7 @@ import App from './App'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(
-    <Router>
-      {' '}
-      <App />{' '}
-    </Router>,
-    div
-  )
+  ReactDOM.render(<Router><App /></Router>, div)
+})
+
 })
