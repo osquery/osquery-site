29391c493a814e0c69b0bf2abac13e9b2f726de4
Zach Wasserman <zach@fleetdm.com>
2022-06-22 12:38:56 -0700

    Disable service worker
    This will hopefully fix the issue of the website showing an empty white
    screen until a hard refresh is performed after an update is deployed.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import registerServiceWorker from './registerServiceWorker'
 import 'react-select/dist/react-select.css'
 
 import './index.css'
@@ -8,4 +7,3 @@ import './index.css'
 import Router from 'Router'
 
 ReactDOM.render(<Router />, document.getElementById('root'))
-registerServiceWorker()
